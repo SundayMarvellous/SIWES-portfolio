53d83f5e1f209380bfd7844e7773fe3070a3b6a2
SundayMarvellous <114695884+SundayMarvellous@users.noreply.github.com>
2023-09-08 15:51:45 +0100

    styling

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import Nav from './Nav'
 import Perfect from '../images/home.jpg';
 import Navbottom from './Navbottom';
+import { useNavigate } from "react-router-dom";
+
 
 function Home() {
+  const navigate = useNavigate();
+  const handleToContact = () => {
+    navigate("/contact");
+  };
   return (
     <div className='home'>
       <Nav />
@@ -14,6 +20,11 @@ function Home() {
       <span className='intro-explain'>This is a portfolio containing the projects I carried out during my 6 months SIWES. </span>
       </p>
       <img src={Perfect} alt='picture'/>
+      </div>
+      <div className='getintouch'>
+        <p>Interested in <br />working together?</p>
+        <hr />
+        <button onClick={handleToContact}>GET IN TOUCH</button>
       </div>
       <Navbottom />
     </div>
