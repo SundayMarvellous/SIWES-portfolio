f8cd30a73117d1cf3f5c10052fd9bf125b81c3bb
SundayMarvellous <114695884+SundayMarvellous@users.noreply.github.com>
2023-09-11 12:17:55 +0100

    type writer effect package

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Nav from './Nav'
 import Navbottom from './Navbottom';
 import Aboutpic from '../images/aboutpic3.jpg';
+import Typewriter from './Typewriter';
 
 function About() {
   return (
@@ -11,6 +12,7 @@ function About() {
         <img src={Aboutpic} alt='mypicture' />
         <div className='main-text'> 
         <h1 className='head'>ABOUT</h1>
+        <p><Typewriter text="My React App" delay={100} infinite /></p>
         </div>
      
         
--- portfolio/src/components/Typewriter.jsx
+++ portfolio/src/components/Typewriter.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
 
-function Typewriter() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const Typewriter = ({ text, delay, infinite }) => {
+  const [currentText, setCurrentText] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-export default Typewriter
+  useEffect(() => {
+    let timeout;
+
+    if (currentIndex <= text.length) {
+      timeout = setTimeout(() => {
+        setCurrentText(prevText => prevText + text[currentIndex]);
+        setCurrentIndex(prevIndex => prevIndex + 1);
+      }, delay);
+
+    } else if (infinite) { // ADD THIS CHECK
+      setCurrentIndex(0);
+      setCurrentText('');
+    }
+
+    return () => clearTimeout(timeout);
+  }, [currentIndex, delay, infinite, text]);
+
+  return <span>{currentText}</span>;
+};
+
+export default Typewriter;
