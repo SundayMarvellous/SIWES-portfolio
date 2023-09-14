12e8c20cfea9e0ec2dec12f63b7e441aef0b6df1
SundayMarvellous <114695884+SundayMarvellous@users.noreply.github.com>
2023-09-14 12:15:30 +0100

    styling form

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -21,23 +21,7 @@ function Contact() {
         >
           <LazyLoadImage img src={Bg} className="image" />
         </div>
-        <form>
-          <h1>Like what you see?</h1>
-          <h3>Let's get in touch!</h3>
-          <p>
-            Please let me know what you're working on and how I might be of
-            assistance. I'm searching for a new position right now and I am open
-            to a variety of alternatives. I am interested in working remotely.
-            However, I'm equally delighted to learn about chances that don't
-            fall within that category. I'm a dedicated and upbeat individual who
-            will tackle every activity with a feeling of purpose and close
-            attention to the details. Please feel free to look at the websites
-            I've listed below and to get in touch with me using the form.
-          </p>
-          <div className="formdetails">
-            <Form />
-          </div>
-        </form>
+          <Form />
       </div>
       <Navbottom />
     </div>
--- portfolio/src/components/Form.jsx
+++ portfolio/src/components/Form.jsx
@@ -2,25 +2,40 @@ import React from 'react'
 
 function Form() {
   return (
-    <div className='form-details'>
-        <div>
-            <label>Name: <br />
-             <input type="text" placeholder="David Stephenson"/>
-            </label> 
+    <form>
+        <h1>Like what you see?</h1>
+        <h3>Let's get in touch!</h3>
+        <p>
+        Please let me know what you're working on and how I might be of
+        assistance. I'm searching for a new position right now and I am open
+        to a variety of alternatives. I am interested in working remotely.
+        However, I'm equally delighted to learn about chances that don't
+        fall within that category. I'm a dedicated and upbeat individual who
+        will tackle every activity with a feeling of purpose and close
+        attention to the details. Please feel free to look at the websites
+        I've listed below and to get in touch with me using the form.
+        </p>
+        <div className='form-details'>
+            <div>
+                <label>Name: <br />
+                <input type="text" placeholder="David Stephenson"/>
+                </label> 
+            </div>
+            <div>
+                <label>Email:<br />
+                <input type="text" placeholder="davidstephenson@gmail.com"/>
+                </label>
+            </div>
+            <div>
+                <label>Message:<br />
+                    <textarea type="text" placeholder="Type your Message here"/>
+                </label>
+            </div>
+            <button type='submit'>SUBMIT</button>
+            
         </div>
-        <div>
-            <label>Email:<br />
-             <input type="text" placeholder="davidstephenson@gmail.com"/>
-            </label>
-        </div>
-        <div>
-            <label>Message:<br />
-                <textarea type="text" placeholder="Type your Message here"/>
-            </label>
-        </div>
-        <button type='submit'>SUBMIT</button>
-        
-    </div>
+  </form>
+    
   )
 }
 
