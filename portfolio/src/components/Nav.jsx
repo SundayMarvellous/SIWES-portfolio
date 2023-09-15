f2e1425c9caa7fca0e122a146d321308104a27d2
SundayMarvellous <114695884+SundayMarvellous@users.noreply.github.com>
2023-09-15 13:35:20 +0100

    hamburger

--- portfolio/src/components/Nav.jsx
+++ portfolio/src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
 
@@ -16,18 +16,37 @@ function Nav() {
   const handleToContact = () => {
     navigate("/contact");
   };
+  const [isShown, setIsShown] = useState(false);
 
-
+  const hamburger = () => {
+    setIsShown(current => !current);
+  }
   return (
     <div className='Nav'>
       <nav>
         <h1>SM</h1>
-        <ul>
+        <ul className='navbar'>
           <span className="Formlink" onClick={handleToHome}><li>HOME</li></span>
           <span className="Formlink" onClick={handleToAbout}><li>ABOUT</li></span>
           <span className="Formlink" onClick={handleToPortfolio}><li>PORTFOLIO</li></span>
           <span className="Formlink" onClick={handleToContact}><li>CONTACT ME</li></span>
         </ul>
+        <div class="hamburger" onClick={hamburger}>
+          <div>
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+          </div>
+        </div>
+        {isShown && (
+        <ul className='sidebar'>
+          <span className="Formlink" onClick={handleToHome}><li>HOME</li></span>
+          <span className="Formlink" onClick={handleToAbout}><li>ABOUT</li></span>
+          <span className="Formlink" onClick={handleToPortfolio}><li>PORTFOLIO</li></span>
+          <span className="Formlink" onClick={handleToContact}><li>CONTACT ME</li></span>
+        </ul>
+         )}
+          
       </nav>
     </div>
    
